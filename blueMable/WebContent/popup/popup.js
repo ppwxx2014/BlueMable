44ca831b22b3bdce951b6a54f9dfc3e4e4047ed5
ppwxx2014 <ppwxx2014@naver.com>
2019-11-06 18:19:42 +0900

    owner분기 설정

--- blueMable/WebContent/popup/popup.js
+++ blueMable/WebContent/popup/popup.js
@@ -1,6 +1,8 @@
-function buy_popup(el, title, value){//증서구입
-
-    var $el = $(el);        //레이어의 id를 $el 변수에 저장
+// title = 도시이름
+// value = 부지 처음 살떄 땅값
+function buy_popup(title, value){ //증서구입
+	console.log("함수실행");
+    var $el  = $("#buyLandLayer");        //레이어의 id를 $el 변수에 저장
     //var isDim = $el.prev().hasClass('dimBg');   //dimmed 레이어를 감지하기 위한 boolean 변수
 
     //isDim ? $('.dim-layer').fadeIn() : $el.fadeIn();
@@ -41,9 +43,9 @@ function buy_popup(el, title, value){//증서구입
     });
     */
 }
-function build_popup(el, title){//건물 구입
+function build_popup(title){//건물 구입
 
-    var $el = $(el);        //레이어의 id를 $el 변수에 저장
+    var $el = $("#buildLayer");        //레이어의 id를 $el 변수에 저장
     $('#dim-layer-build').fadeIn();
     var $elWidth = ~~($el.outerWidth()),//팝업의 넓이
         $elHeight = ~~($el.outerHeight()),//팝업의 높이
@@ -75,9 +77,9 @@ function build_popup(el, title){//건물 구입
 
 }
 
-function Loan_popup(el){//대출
+function Loan_popup(){//대출
 
-    var $el = $(el);        //레이어의 id를 $el 변수에 저장
+    var $el = $("#loanLayer");        //레이어의 id를 $el 변수에 저장
     $('#dim-layer-loan').fadeIn();
     var $elWidth = ~~($el.outerWidth()),//팝업의 넓이
         $elHeight = ~~($el.outerHeight()),//팝업의 높이
@@ -97,6 +99,7 @@ function Loan_popup(el){//대출
     $el.find('#buy').click(function(){
         
     	$('#dim-layer-loan').fadeOut();
+		$('#dim-layer-select').fadeOut();
     	
         return true;
     });
@@ -107,9 +110,9 @@ function Loan_popup(el){//대출
 
 }
 
-function Info_popup(el, title){//땅 클릭
+function Info_popup(title){ //땅 클릭
 
-    var $el = $(el);        //레이어의 id를 $el 변수에 저장
+    var $el = $("#informationLayer");        //레이어의 id를 $el 변수에 저장
     $('#dim-layer-Info').fadeIn();
     var $elWidth = ~~($el.outerWidth()),//팝업의 넓이
         $elHeight = ~~($el.outerHeight()),//팝업의 높이
@@ -133,11 +136,10 @@ function Info_popup(el, title){//땅 클릭
     	
         return true;
     });
-
 }
-function Gold_popup(el){//황금카드
+function Gold_popup(){//황금카드
 
-    var $el = $(el);        //레이어의 id를 $el 변수에 저장
+    var $el = $("#goldLayer");        //레이어의 id를 $el 변수에 저장
    
     $('#dim-layer-gold').fadeIn();
     var $elWidth = ~~($el.outerWidth()),//팝업의 넓이
@@ -157,15 +159,15 @@ function Gold_popup(el){//황금카드
     $('.dim-layer').click(function(){
     	
     	$('#dim-layer-gold').fadeOut();
-    	
+    	drowKey();
         return true;
     });
 
 }
 
-function Sale_popup(el){//매각
+function Sale_popup(){//매각
 
-    var $el = $(el);        //레이어의 id를 $el 변수에 저장
+    var $el = $("#saleLayer");        //레이어의 id를 $el 변수에 저장
    
     $('#dim-layer-sale').fadeIn();
     var $elWidth = ~~($el.outerWidth()),//팝업의 넓이
@@ -185,8 +187,41 @@ function Sale_popup(el){//매각
     $el.find('#buy').click(function(){
         
     	$('#dim-layer-loan').fadeOut();
-    	
+
+		$('#dim-layer-select').fadeOut();
         return true;
     });
+
+}
+
+function SelectLoanSale_popup(){//매각
+
+    var $el = $("#saleLayer");        //레이어의 id를 $el 변수에 저장
+   
+    $('#dim-layer-select').fadeIn();
+    var $elWidth = ~~($el.outerWidth()),//팝업의 넓이
+        $elHeight = ~~($el.outerHeight()),//팝업의 높이
+        docWidth = $(document).width(),//브라우저 창 넓이
+        docHeight = $(document).height(); //브라우저 창 높이
+
+    // 화면의 중앙에 레이어를 띄운다.
+    if ($elHeight < docHeight || $elWidth < docWidth) {
+        $el.css({
+            marginTop: -$elHeight /2,
+            marginLeft: -$elWidth/2
+        })
+    } else {//팝업창이 화면보다 클경우 마진을 지움
+        $el.css({top: 0, left: 0});
+    }
+	$el.find('#loan').click(function() {
+		Loan_popup();
+
+		return true;
+	});
+	$el.find('#sale').click(function() {
+		Sale_popup();
+
+		return true;
+	});
 
 }
